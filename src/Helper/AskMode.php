ef7fe7c118109c4e11b8ec72e14005934e207b69
Ryan King <ryan.king@gosynesis.com>
2016-08-05 14:17:51 -0700

    New helper class for valid ask modes

---
+++ src/Helper/AskMode.php
@@ -0,0 +1,25 @@
+<?php
+    /**
+     * This file contains PHP classes that can be used to interact with the Tropo WebAPI/
+     *
+     * @see       https://www.tropo.com/docs/webapi/
+     *
+     * @copyright 2010 Mark J. Headd (http://www.voiceingov.org)
+     * @package   TropoPHP
+     * @author    Mark Headd
+     * @author    Adam Kalsey
+     */
+    namespace Tropo\Helper;
+
+    /**
+     * AskMode Helper class.
+     *
+     * @package Tropo\Helper
+     */
+    class AskMode {
+
+        const ANY    = "any";
+        const DTMF   = "dtmf";
+        const SPEECH = "speech";
+
+    }
