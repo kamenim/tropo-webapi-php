a6e72b4d7a68e77b2995dd64196e3be4bdb93661
Ryan King <ryan.king@gosynesis.com>
2016-08-05 14:19:05 -0700

    Updating to new standards

--- src/Action/Result.php
+++ src/Action/Result.php
@@ -16,34 +16,52 @@
     /**
      * Returned anytime a request is made to the Tropo Web API.
      *
-     * @package TropoPHP
+     * @package Tropo\Action
      *
      */
     class Result {
 
-        private $_sessionId;
-        private $_callId;
-        private $_state;
-        private $_sessionDuration;
-        private $_sequence;
-        private $_complete;
-        private $_error;
+        /** @var array|\stdClass */
         private $_actions;
-        private $_name;
+        /** @var  integer */
         private $_attempts;
-        private $_disposition;
-        private $_confidence;
-        private $_interpretation;
+        /** @var  string */
+        private $_callId;
+        /** @var  boolean */
+        private $_complete;
+        /** @var null|string */
         private $_concept;
-        private $_userType;
-        private $_utterance;
-        private $_value;
+        /** @var  integer */
+        private $_confidence;
+        /** @var  string */
+        private $_disposition;
+        /** @var  string */
+        private $_error;
+        /** @var  string */
+        private $_interpretation;
+        /** @var  string */
+        private $_name;
+        /** @var  integer */
+        private $_sequence;
+        /** @var  integer */
+        private $_sessionDuration;
+        /** @var  string */
+        private $_sessionId;
+        /** @var  string */
+        private $_state;
+        /** @var null|string Undocumented */
         private $_transcription;
+        /** @var  string */
+        private $_userType;
+        /** @var  string */
+        private $_utterance;
+        /** @var  string */
+        private $_value;
 
         /**
-         * Class constructor
+         * Result constructor.
          *
-         * @param string $json
+         * @param null|string $json
          *
          * @throws \Tropo\Exception\TropoException
          */
@@ -80,75 +98,129 @@
             $this->_transcription   = isset($result->result->transcription) ? $result->result->transcription : null;
         }
 
-        public function getSessionId () {
-            return $this->_sessionId;
-        }
-
-        public function getCallId () {
-            return $this->_callId;
-        }
-
-        public function getState () {
-            return $this->_state;
-        }
-
-        public function getSessionDuration () {
-            return $this->_sessionDuration;
-        }
-
-        public function getSequence () {
-            return $this->_sequence;
-        }
-
-        public function isComplete () {
-            return (bool)$this->_complete;
-        }
-
-        public function getError () {
-            return $this->_error;
-        }
-
-        public function getUserType () {
-            return $this->_userType;
-        }
-
+        /**
+         * @return array|\stdClass
+         */
         public function getActions () {
             return $this->_actions;
         }
 
-        public function getName () {
-            return $this->_name;
-        }
-
+        /**
+         * @return integer
+         */
         public function getAttempts () {
             return $this->_attempts;
         }
 
-        public function getDisposition () {
-            return $this->_disposition;
+        /**
+         * @return string
+         */
+        public function getCallId () {
+            return $this->_callId;
         }
 
-        public function getConfidence () {
-            return $this->_confidence;
-        }
-
-        public function getInterpretation () {
-            return $this->_interpretation;
-        }
-
+        /**
+         * @return null|string
+         */
         public function getConcept () {
             return $this->_concept;
         }
 
+        /**
+         * @return integer
+         */
+        public function getConfidence () {
+            return $this->_confidence;
+        }
+
+        /**
+         * @return string
+         */
+        public function getDisposition () {
+            return $this->_disposition;
+        }
+
+        /**
+         * @return string
+         */
+        public function getError () {
+            return $this->_error;
+        }
+
+        /**
+         * @return string
+         */
+        public function getInterpretation () {
+            return $this->_interpretation;
+        }
+
+        /**
+         * @return string
+         */
+        public function getName () {
+            return $this->_name;
+        }
+
+        /**
+         * @return integer
+         */
+        public function getSequence () {
+            return $this->_sequence;
+        }
+
+        /**
+         * @return integer
+         */
+        public function getSessionDuration () {
+            return $this->_sessionDuration;
+        }
+
+        /**
+         * @return string
+         */
+        public function getSessionId () {
+            return $this->_sessionId;
+        }
+
+        /**
+         * @return string
+         */
+        public function getState () {
+            return $this->_state;
+        }
+
+        /**
+         * @return null|string
+         */
+        public function getTranscription () {
+            return $this->_transcription;
+        }
+
+        /**
+         * @return string
+         */
+        public function getUserType () {
+            return $this->_userType;
+        }
+
+        /**
+         * @return string
+         */
         public function getUtterance () {
             return $this->_utterance;
         }
 
+        /**
+         * @return string
+         */
         public function getValue () {
             return $this->_value;
         }
 
-        public function getTranscription () {
-            return $this->_transcription;
+        /**
+         * @return boolean
+         */
+        public function isComplete () {
+            return (bool)$this->_complete;
         }
     }
